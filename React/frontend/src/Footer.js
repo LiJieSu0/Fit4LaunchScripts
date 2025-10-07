e3d3f8a4d8ad71184377d1667b00f21f1adff991
LiJie Su <opwinopwin@gmail.com>
2025-10-07 14:13:54 -0500

    add page footer

--- React/frontend/src/Footer.js
+++ React/frontend/src/Footer.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer className="bg-white shadow-md mt-8">
-      <div className="container mx-auto px-4 py-4">
-        <div className="flex items-center justify-between text-sm text-gray-600">
-          <span>Proprietary & Confidential</span>
-          <span>spirent.com | 4</span>
-        </div>
-      </div>
+    <footer className="print-footer">
+      {/* Your footer content here */}
+      <p>Page Footers</p>
     </footer>
   );
-}
+};
 
 export default Footer;
