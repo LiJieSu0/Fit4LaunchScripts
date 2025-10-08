a6cd10f0b4a05de0a586a04ee8dea34bccbf3991
LiJie Su <opwinopwin@gmail.com>
2025-10-08 16:39:42 -0500

    add header

--- React/frontend/src/Header.js
+++ React/frontend/src/Header.js
@@ -3,17 +3,13 @@ import React from 'react';
 function Header() {
   return (
     <header className="bg-[#f0f0f0] shadow-lg print-header">
-      <div className="container mx-auto px-4 py-4">
+      <div className="container mx-auto px-4 py-1">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
-            <img src="/logo192.png" alt="ATMCL Logo" className="h-10 mr-4" /> {/* Placeholder for ATMCL logo */}
+            <img src="/atmclogo.jpg" alt="ATMCL Logo" className="h-10 mr-4" /> {/* Placeholder for ATMCL logo */}
             <div>
-              <h1 className="text-xl font-bold text-gray-800">ATMCL Field Performance Test Report</h1>
-              <p className="text-xs text-gray-600">Samsung Galaxy Fold S34</p>
+              <h1 className="text-xl font-bold text-gray-800">Field Test Report-Samsung Galaxy Fold S34</h1>
             </div>
-          </div>
-          <div>
-            <img src="/ATMCL-logo.png" alt="ATMCL" className="h-10" /> {/* Placeholder for ATMCL logo on the right */}
           </div>
         </div>
       </div>
