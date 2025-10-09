41e971b7b8c89153a88037f895e55d49972fad06
LiJie Su <opwinopwin@gmail.com>
2025-10-09 04:44:22 -0500

    finish data performance general cases

--- React/frontend/src/DataPerformanceTables/HTTPSSUL.js
+++ React/frontend/src/DataPerformanceTables/HTTPSSUL.js
@@ -14,39 +14,39 @@ const HttpssULTable = () => {
         <tbody>
           <tr className="bg-yellow-50">
             <td className="py-2 px-4 border border-table-grid text-center align-middle">5G Auto Data Test Stationary Location 1</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-pass)' }}>89.75</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-pass)' }}>85.15</td>
 
           </tr>
           <tr className="bg-yellow-50">
             <td className="py-2 px-4 border border-table-grid text-center align-middle">5G Auto Data Test Stationary Location 2</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold" style={{ backgroundColor: 'var(--performance-fail)' }}>4.14</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold" style={{ backgroundColor: 'var(--performance-fail)' }}>10.35</td>
           </tr>
           <tr className="bg-yellow-50">
             <td className="py-2 px-4 border border-table-grid text-center align-middle">5G Auto Data Test Stationary Location 3</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-excellent)' }}>31.85</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-excellent)' }}>28.92</td>
           </tr>
           <tr className="bg-yellow-50">
             <td className="py-2 px-4 border border-table-grid text-center align-middle">5G Auto Data Test MHS Stationary Location 1</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-pass)' }}>25.54</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-pass)' }}>23.59</td>
           </tr>
           <tr className="bg-yellow-50">
             <td className="py-2 px-4 border border-table-grid text-center align-middle">5G Auto Data Test MHS Stationary Location 2</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-fail)' }}>2.59</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-fail)' }}>10.24</td>
           </tr>
           <tr className="bg-yellow-50">
             <td className="py-2 px-4 border border-table-grid text-center align-middle">5G NSA Data Test Stationary Moderate RF</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-fail)' }}>4.71</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-fail)' }}>11.60</td>
           </tr>
           <tr className="bg-yellow-50">
             <td className="py-2 px-4 border border-table-grid text-center align-middle">5G NSA Data Test Stationary Poor RF</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
-            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold">Results</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-excellent)' }}>43.69</td>
+            <td className="py-2 px-4 border border-table-grid text-center bg-yellow-50 text-black-700 font-bold"style={{ backgroundColor: 'var(--performance-excellent)' }}>27.71</td>
           </tr>
         </tbody>
       </table>
